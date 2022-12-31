fc36007b6d4bf36e88ea4da4962a1a4285f623e1
Veronique Cadet <cadetvero@gmail.com>
2022-12-30 20:44:04 -0500

    basic setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <div className="div">
+      <h1>Zenned</h1>
+      <p>Stats</p>
+      <p>Stats</p>
+      <p>Stats</p>
+    </div>
+  );
+};
+
+export default NavBar;
