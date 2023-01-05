32b452fa81e4d5ed0e22013d33606f29793151be
Olivia Radick <olivia.radick@gmail.com>
2023-01-05 12:38:22 -0500

    some button styling

--- src/components/SelectTimeBar.jsx
+++ src/components/SelectTimeBar.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const SelectTimeBar = ()=>{
   return (
     <div>
-      <h2 id='olivia'>
-        <Link to="/stats/daily-stats">
+      <h2 id='olivia'className='time-option'>
+        <Link to="/stats/daily-stats" className='time-item'>
           DAILY
         </Link>
-        <Link to="/stats/weekly-stats">
+        <Link to="/stats/weekly-stats" className='time-item'>
           WEEKLY      
         </Link>
-        <Link to="/stats/monthly-stats">
+        <Link to="/stats/monthly-stats" className='time-item'>
           MONTHLY
         </Link>
       </h2>
--- src/components/WeeklyStats.jsx
+++ src/components/WeeklyStats.jsx
@@ -8,14 +8,35 @@ import WeeklyMealGraph from "./WeeklyMealGraph";
 const WeeklyStats = ({logs}) => {
 
   const [chosenWeeklyChart, setChosenWeeklyChart] = useState("default")
+  // const [buttonClass, setButtonClass] = useState("select-category")
 
   return (
     <div>
-      <div>
-        <button onClick={()=>setChosenWeeklyChart(<WeeklySleepGraph logs={logs}/>)}>SLEEP</button>
-        <button onClick={()=>setChosenWeeklyChart(<WeeklyMoodPie logs={logs}/>)}>MOOD</button>
-        <button onClick={()=>setChosenWeeklyChart(<WeeklyWaterGraph logs={logs}/>)}>WATER</button>
-        <button onClick={()=>setChosenWeeklyChart(<WeeklyMealGraph logs={logs}/>)}>MEALS</button>
+      <div className="category-bar">
+        <h2>Your Weekly Digest:</h2>
+        <button 
+          className= "select-category"
+          onClick={()=>{setChosenWeeklyChart(<WeeklySleepGraph logs={logs}/>)
+          // {buttonClass ="select-category" ? (setButtonClass("selected-category")) : (setButtonClass("select-category"))} 
+          //   
+          }}
+          >SLEEP
+        </button>
+        <button 
+          className="select-category" 
+          onClick={()=>setChosenWeeklyChart(<WeeklyMoodPie logs={logs}/>)}
+          >MOOD
+        </button>
+        <button 
+          className="select-category" 
+          onClick={()=>setChosenWeeklyChart(<WeeklyWaterGraph logs={logs}/>)}
+          >WATER
+        </button>
+        <button 
+          className="select-category" 
+          onClick={()=>setChosenWeeklyChart(<WeeklyMealGraph logs={logs}/>)}
+          >MEALS
+        </button>
       <div/>
       </div>
        {chosenWeeklyChart=== "default" ? <WeeklySleepGraph logs={logs}/> : chosenWeeklyChart}          
