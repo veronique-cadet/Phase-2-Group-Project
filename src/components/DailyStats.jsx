4b0adb2594cd93979b39cca5d419e8f0783248e6
Veronique Cadet <cadetvero@gmail.com>
2023-01-05 12:30:16 -0500

    cards css

--- src/components/DailyStats.jsx
+++ src/components/DailyStats.jsx
@@ -3,17 +3,31 @@ import { useState } from "react";
 const DailyStats = ({ log, date }) => {
   return (
     <div className="daily-stats">
-      <strong>
-        <p>
-          {date.month} {date.day},{date.year}
+      <p className="daily-date-header">
+        {date.month} {date.day},{date.year}
+      </p>
+      <div className="daily-info_containter">
+        <p className="daily-keys">
+          {" "}
+          <p className="key">Sleep:&nbsp;</p> {log.sleep} hours
         </p>
-      </strong>
-      <p>Sleep: {log.sleep} hours</p>
-      <p>Water: {log.water} cups</p>
-      <p>Meals: {log.meals} meals</p>
-      <p>Exercied? {log.exercise ? "Yes" : "No"}</p>
-      <p>Meditated? {log.meditation ? "Yes" : "No"}</p>
-      <p>Mood: {log.mood}/5</p>
+        <p className="daily-keys">
+          <p className="key">Water:&nbsp;</p> {log.water} cups
+        </p>
+        <p className="daily-keys">
+          <p className="key">Meals:&nbsp;</p> {log.meals} meals
+        </p>
+        <p className="daily-keys">
+          <p className="key">Exercied?&nbsp;</p> {log.exercise ? "Yes" : "No"}
+        </p>
+        <p className="daily-keys">
+          <p className="key">Meditated?&nbsp;</p>{" "}
+          {log.meditation ? "Yes" : "No"}
+        </p>
+        <p className="daily-keys">
+          <p className="key">Mood:&nbsp;</p> {log.mood}/5
+        </p>
+      </div>
     </div>
   );
 };
